b771c56403ecefa8356200984b8708f6ea3a5082
Sumitha Naidu <sumithanaidu1996@gmail.com>
2022-12-07 21:17:30 +0530

    Created react app - Digital kadai

---
+++ digital-kadai/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Digital Kadai</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
