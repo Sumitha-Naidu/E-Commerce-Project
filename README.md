8780726bf87613fe23a6a64c2cdaa1a774ead2b5
Sumitha Naidu <sumithanaidu1996@gmail.com>
2022-12-07 22:56:23 +0530

    Display the Products

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. Create React App
 2. Create Git Repository
+3. Display the Products
--- digital-kadai/src/App.js
+++ digital-kadai/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +8,33 @@ function App() {
       <header>
         <a href="/">Digital Kadai</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div
+              className="product"
+              key={product.slug}
+            >
+              <a href={`/product/${product.slug}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
